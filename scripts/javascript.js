9439326e615d5ec5952e37eef2d456dce26d6df0
Juli <jarivero@fi.uba.ar>
2024-07-16 00:01:54 -0300

    mostrar btnLogin solo si no hay token guardado

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -1,4 +1,24 @@
-function login() {
-    document.getElementById('login_section').classList = 'hidden';
-    document.getElementById('avatar_section').classList = 'end-p1';
-}
+const ACCESS_TOKEN_KEY = "access_token_fi_share";
+
+document.addEventListener("DOMContentLoaded", async () => {
+  const access_token = localStorage.getItem(ACCESS_TOKEN_KEY);
+  if (access_token) {
+    try {
+        //obtener datos y mostrarlos
+    } catch (error) {
+      console.error(error);
+    }
+  } else { // Colocar boton de iniciar sesion en header
+    const btnLogin = document.createElement("button");
+    btnLogin.className = "ml-auto dark:bg-[#24292F] font-medium rounded-lg text-sm px-3 py-0 text-center inline-flex items-center dark:text-slate-400 hover:ring-2 dark:hover:ring-gray-500 transition duration-300 ease-in-out";
+    btnLogin.innerHTML = `
+      Iniciar sesion con GitHub
+      <img src="/images/github.svg" alt="GitHub Logo"  class="w-4 h-4 invert ml-2">
+      `;
+    // Asignar funcion de redirigir a github
+    btnLogin.addEventListener("click", () => {
+        //...
+    });
+    document.querySelector("header").appendChild(btnLogin);
+  }
+});
