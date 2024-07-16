027696e447af0c6f3eb5e75cd62d03ff0495ad26
Carolina Amarilla <Carolina Amarilla>
2024-07-16 15:37:46 -0300

    Agrego control de los items que se muestran de la barra de navegacion, segun la pagina actual -El codigo se ejecuta cuando la pagina se carga completamente -Dependiendo de la pagina actual, se muestran y ocultan los enlaces de navegacion, se muestran los nombres de las materias,cursos y/o tps elegidos -Si falta alguna informacion en la URL, te redirige a la pagina de la informacion faltante

--- scripts/javascript.js
+++ scripts/javascript.js
@@ -25,3 +25,66 @@ document.addEventListener("DOMContentLoaded", async () => {
     document.querySelector("header").appendChild(btnLogin);
   }
 });
+
+function login() {
+    document.getElementById('login_section').classList = 'hidden';
+    document.getElementById('avatar_section').classList = 'end-p1';
+}
+
+document.addEventListener('DOMContentLoaded', function() {
+    const pagina_actual = window.location.pathname;
+    const paramentros = new URLSearchParams(window.location.search);
+    
+    if(pagina_actual === '/'){
+      document.getElementById('nav_materia').classList = 'hidden';
+      document.getElementById('nav_curso').classList = 'hidden';
+      document.getElementById('nav_tp').classList = 'hidden';
+      document.getElementById('nav_repo').classList = 'hidden';
+    }
+    else if(pagina_actual === '/cursos/'){
+      document.getElementById('curso').classList = 'ms-1 text-sm font-medium dark:text-gray-400'
+      document.getElementById('curso').removeAttribute('href');
+      document.getElementById('nav_tp').classList = 'hidden';
+      document.getElementById('nav_repo').classList = 'hidden';
+  
+      const materia = paramentros.get('materia');
+      if(materia === null){
+        window.location.href = '/#materias';
+      }
+      document.getElementById('materia').innerText = materia;
+    }
+    else if(pagina_actual === '/tps/'){
+      document.getElementById('tp').classList = 'ms-1 text-sm font-medium dark:text-gray-400'
+      document.getElementById('tp').removeAttribute('href');
+      document.getElementById('nav_repo').classList = 'hidden';
+  
+      const materia = paramentros.get('materia');
+      if(materia === null){
+        window.location.href = '/#materias';
+      }
+      document.getElementById('materia').innerText = materia;
+      const curso = paramentros.get('curso');
+      if(curso === null){
+        window.location.href = `/cursos/?materia=${materia}`;
+      }
+      document.getElementById('curso').innerText = curso;
+    }
+    else if(pagina_actual === '/repos/'){
+      const materia = paramentros.get('materia');
+      if(materia === null){
+        window.location.href = '/#materias';
+      }
+      document.getElementById('materia').innerText = materia;
+      const curso = paramentros.get('curso');
+      if(curso === null){
+        window.location.href = `/cursos/?materia=${materia}`;
+      }
+      document.getElementById('curso').innerText = curso;
+      const tp = paramentros.get('tp');
+      if(tp === null){
+        window.location.href = `/tps/?materia=${materia}&curso=${curso}`;
+      }
+      document.getElementById('tp').innerText = tp;
+      
+    }
+});
