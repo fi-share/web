41bb0a4c7ad48656a75cc97f476e244ec2e099e9
Juli <jarivero@fi.uba.ar>
2024-07-17 16:29:48 -0300

    details perfil con datos de prueba

--- scripts/login.js
+++ scripts/login.js
@@ -2,9 +2,39 @@ import {ACCESS_TOKEN_KEY, CLIENT_ID} from "../config.js"
 
 document.addEventListener("DOMContentLoaded", async () => {
   const access_token = localStorage.getItem(ACCESS_TOKEN_KEY);
-  if (access_token) {
+  if (access_token) { //obtener datos y mostrarlos
     try {
-      //obtener datos y mostrarlos
+      const avatar_url = "/images/icono.jpeg";
+      const login = "fi-share";
+      const name = "";
+      const html_url = "https://github.com/fi-share";
+
+      const containerPerfil = document.createElement("details");
+      containerPerfil.className = "relative";
+      containerPerfil.role = "container-perfil"
+      containerPerfil.innerHTML = `
+      <summary class="list-none cursor-pointer">
+        <img src="${avatar_url}" alt="avatar" class="w-10 h-10 rounded-full shadow-2xl p-1">
+      </summary>
+      <section class="absolute w-36 text-center right-full -translate-x-4 translate-y-4 text-indigo-900">
+        <ul role="minidata-list" 
+        class="border border-indigo-900 rounded-t-lg mb-2 p-2 first:text-lg first:font-bold [&:not(:first-child)]:text-xs [&:not(:first-child)]:font-light">
+          ${(name ? `<li class="text-nowrap">${name}</li>` : "")}
+          <li class="text-nowrap">${login}</li>
+        </ul>
+        <nav class="border border-indigo-900 rounded-b-lg py-3">
+          <a href="${html_url}" target="_blank" href="#" 
+            class="block p-2 hover:font-black">
+            Mi GitHub
+          </a>
+          <button id="cerrar-sesion" 
+            class="block w-full p-2 hover:font-bold">
+            Cerrar Sesión
+          </button>
+        </nav>
+      </section>
+      `
+      document.querySelector("header").appendChild(containerPerfil)
     } catch (error) {
       console.error(error);
     }
