b442660cea19c67fb18476c89e1bf44bc8c941e1
Juli <jarivero@fi.uba.ar>
2024-07-17 21:03:19 -0300

    funcionalidad resizer-x (redireccionar espacio horizontal de un contenedor), aplicado en el aside de '/repos'

---
+++ scripts/resizer.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', () => {
+    const resizable = document.querySelector('.resizable-x');
+    const resizer = resizable.querySelector('.resizer-right');
+
+    let isResizing = false;
+
+    function resize(e) {
+        if (isResizing) {
+            resizable.style.width = `${e.pageX - resizable.getBoundingClientRect().left}px`;
+        }
+    }
+    
+    function stopResize() {
+        if (isResizing) {
+            document.removeEventListener('mousemove', resize);
+            document.removeEventListener('mouseup', stopResize);
+            isResizing = false;
+        }
+    }
+    
+    resizer.addEventListener('mousedown', (e) => {
+        isResizing = true;
+        document.addEventListener('mousemove', resize);
+        document.addEventListener('mouseup', stopResize);
+    });
+});
