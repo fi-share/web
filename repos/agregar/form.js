95ec565168f4a36d85e6f8e9c26ee83cc697108d
Juli <jarivero@fi.uba.ar>
2024-07-19 17:45:16 -0300

    solicitud al backend (aun no testeada)

--- repos/agregar/form.js
+++ repos/agregar/form.js
@@ -1,9 +1,16 @@
-import {ACCESS_TOKEN_KEY} from "../../config.js"
+import {ACCESS_TOKEN_KEY, URL_API} from "../../config.js"
+
+const params = new URLSearchParams(location.search);
 
 document.addEventListener("DOMContentLoaded", async () => {
     const access_token = localStorage.getItem(ACCESS_TOKEN_KEY);
     if (!access_token) { 
         alert("Debe iniciar sesión")
+        location.href = location.origin;
+    }
+    const idTP = params.get("tp-id")
+    if (!idTP) {
+        alert("No se coloco el tp en la url")
         location.href = location.origin;
     }
     const response = await fetch("https://api.github.com/user/repos", {
@@ -16,8 +23,9 @@ document.addEventListener("DOMContentLoaded", async () => {
     const data = await response.json();
     const tempContent = document.getElementById("temp-repo").content
     const fieldSelecRepo = document.getElementById("seleccionar-repo")
+    const idUsuario = document.querySelector('input[type="hidden"][name="id"]').value
     data.forEach(({description, full_name, name, id, owner}) => {
-        if (owner.id == document.querySelector('input[type="hidden"][name="id"]').value) {
+        if (owner.id == idUsuario) {
             console.log(description,full_name,name,id);
             const repo = tempContent.cloneNode(true).firstElementChild;
 
@@ -40,11 +48,21 @@ document.addEventListener("DOMContentLoaded", async () => {
         }
         
     });
-})
+    document.getElementById("form-agregar").addEventListener("submit", async (e) => {
+        e.preventDefault();
+        console.log(e);
+        const formData = new FormData(e.target);
+        formData.append("id_usuario", idUsuario)
+        const resp = await fetch(`${URL_API}/tps/${idTP}/repositorios`, {
+            method: "POST",
+            body: formData,
+        })
+        if (resp.status >= 200 && resp.status < 300) {
+            alert("Agregado correctamente")
 
-document.getElementById("form-agregar").addEventListener("submit", async (e) => {
-    e.preventDefault();
-    console.log(e);
-    const formdata = new FormData(e.target);
-    console.log(formdata);
+        } else {
+            alert("Ocurrió un error")
+        }
+        console.log(formData);
+    })
 })
