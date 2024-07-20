b56cc55b51e772806022aafb71a4c0b3249acf4e
Juli <jarivero@fi.uba.ar>
2024-07-20 13:53:38 -0300

    añadido boton editar en repos en donde el usuario logueado es colaborador o dueño

--- repos/cartas.js
+++ repos/cartas.js
@@ -12,19 +12,31 @@ document.addEventListener("DOMContentLoaded", async () => {
     if (idTP) {
         const resp = await fetch(`${URL_API}/tps/${idTP}`)
         const {repositorios} = await resp.json()
-        console.log(repositorios);
+        const inputHiddenId = document.querySelector('input[type="hidden"][name="id"]');
+        let id_usuario_en_maquina = null
+        if (inputHiddenId) {
+            id_usuario_en_maquina = inputHiddenId.value
+        }
         repositorios.forEach(async ({id, full_name, titulo, descripcion, id_usuario}) => {
             const contributorsResponse = await fetch(`${GITHUB_API_URL}/repos/${full_name}/contributors`, {
                 Accept: "application/vnd.github+json",
                 "X-GitHub-Api-Version": "2022-11-28",
               })
             const contributors = await contributorsResponse.json();
-            console.log("contributors: ", contributorsResponse, contributors);
-
+            
             const carta = tempContent.cloneNode(true).firstElementChild;
             carta.querySelector("#titulo").textContent = titulo;
             carta.querySelector("#descripcion").textContent = descripcion;
             carta.querySelector("#url-repo").setAttribute("href", GITHUB_URL + "/" +  full_name);
+            if ((id_usuario_en_maquina) && (contributors.some(({id})=> id == id_usuario_en_maquina) || id_usuario == id_usuario_en_maquina)) {
+                const button = document.createElement("button");
+                button.className = "absolute right-4 bottom-4";
+                button.innerHTML = `
+                    <img class="w-6 h-6" src="/images/editar.svg" alt="editar">
+                `
+                console.log(button);
+                carta.appendChild(button);
+            }
 
             container.appendChild(carta)
         });
