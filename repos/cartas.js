a0ae2d32716b2dc1c87c29836e26299516bc82e9
Juli <jarivero@fi.uba.ar>
2024-07-21 17:32:46 -0300

    token en headers de los fetch a api de fishare

--- repos/cartas.js
+++ repos/cartas.js
@@ -64,10 +64,12 @@ document.addEventListener("DOMContentLoaded", async () => {
                     `
                     submitBtn.onclick = async () => {
                         const formData = new FormData(form)
-                        formData.append("id_usuario", id_usuario_en_maquina)
                         await fetch(`${URL_API}/tps/${idTP}/repositorios/${id}`, {
                             method: "PUT",
-                            body: formData
+                            body: formData,
+                            headers: {
+                                Authorization: `Bearer ${access_token}`,
+                            } 
                         })
                     }
                     floats.appendChild(submitBtn)
@@ -97,6 +99,15 @@ document.addEventListener("DOMContentLoaded", async () => {
                             <path d="M135.2 17.7L128 32 32 32C14.3 32 0 46.3 0 64S14.3 96 32 96l384 0c17.7 0 32-14.3 32-32s-14.3-32-32-32l-96 0-7.2-14.3C307.4 6.8 296.3 0 284.2 0L163.8 0c-12.1 0-23.2 6.8-28.6 17.7zM416 128L32 128 53.2 467c1.6 25.3 22.6 45 47.9 45l245.8 0c25.3 0 46.3-19.7 47.9-45L416 128z"/>
                         </svg>
                     `
+                    deleteBtn.onclick = async () => {
+                        const formData = new FormData()
+                        await fetch(`${URL_API}/tps/${idTP}/repositorios/${id}`, {
+                            method: "DELETE",
+                            headers: {
+                                Authorization: `Bearer ${access_token}`,
+                            }
+                        })
+                    }
                     floats.appendChild(deleteBtn)
                 }
                 floats.appendChild(editBtn);
