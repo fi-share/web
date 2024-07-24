6fa15e781330098e2bd6987dcb7f6cee0fb70210
Juli <jarivero@fi.uba.ar>
2024-07-24 18:04:17 -0300

    añadido /web a OAUTH_REDIRECT_URI en config

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const ACCESS_TOKEN_KEY = "access_token_fi_share";
 export const URL_API = "https://fi-share-api.onrender.com";
-export const OAUTH_REDIRECT_URI = location.origin + "/github-callback";
+export const OAUTH_REDIRECT_URI = location.origin + "/web/github-callback";
 export const CLIENT_ID = "Ov23liExmnLx6w1ftJPt";
