df86d8d12d7c7971a030a806861c98314e740af3
Rivero Julián <86316267+juli-rivero@users.noreply.github.com>
2024-07-24 17:16:14 -0300

    Update config.js

--- config.js
+++ config.js
@@ -1,4 +1,4 @@
 export const ACCESS_TOKEN_KEY = "access_token_fi_share";
-export const URL_API = "http://localhost:5000";
+export const URL_API = "https://fi-share-api.onrender.com";
 export const OAUTH_REDIRECT_URI = location.origin + "/github-callback";
-export const CLIENT_ID = "Ov23li0fsg8Uodd5Qy1d";
+export const CLIENT_ID = "Ov23liExmnLx6w1ftJPt";
