76e2d37355d9f9a9f599cda877653659fb77dd20
Juli <jarivero@fi.uba.ar>
2024-07-25 01:03:55 -0300

    absoluto anchor en acciones_repos.js

--- repos/acciones_repos.js
+++ repos/acciones_repos.js
@@ -5,7 +5,7 @@ document.addEventListener("DOMContentLoaded", () => {
     if (access_token) {
         const linkNuevo = document.createElement("a")
         linkNuevo.className = "cursor-pointer float-right ml-2 mb-2 w-12 h-12 text-center text-4xl dark:text-gray-400 dark:hover:text-white rounded-lg border border-gray-400 hover:border-white"
-        linkNuevo.setAttribute("href", "web/repos/agregar" + location.search)
+        linkNuevo.setAttribute("href", "/web/repos/agregar" + location.search)
         linkNuevo.textContent = "+"
         
         document.getElementById("title-Repositorios").insertAdjacentElement("beforebegin", linkNuevo)
