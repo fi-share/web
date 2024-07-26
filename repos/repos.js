32171b866f6bcb74ea0257c25a023ecbe7589cfc
Juli <jarivero@fi.uba.ar>
2024-07-26 00:20:08 -0300

    correccion url relativa en repos.js

--- repos/repos.js
+++ repos/repos.js
@@ -16,7 +16,7 @@ function colocar_btn_agregar() {
   const linkNuevo = document.createElement("a");
   linkNuevo.className =
     "cursor-pointer float-right ml-2 mb-2 w-12 h-12 text-center text-4xl dark:text-gray-400 dark:hover:text-white rounded-lg border border-gray-400 hover:border-white";
-  linkNuevo.setAttribute("href", "/repos/agregar" + location.search);
+  linkNuevo.setAttribute("href", "agregar" + location.search);
   linkNuevo.textContent = "+";
 
   document
